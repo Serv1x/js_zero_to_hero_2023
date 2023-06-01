7a175cfbf20ad831b0e2e51ade590a34e2a9c691
Serv1x <tokia@MacBook-Pro.local>
2023-06-01 13:20:10 +0100

    Rnadom challege folder added

---
+++ Rnadom challege/main.js
@@ -0,0 +1,24 @@
+
+// Code for Displaying helloworld using for loop -------------------------------------------------------
+
+let helloWorld = ["H", "E", "L", "L", "O", "W", "O", "R", "L", "D", ]
+
+let n1 = 0
+time = 0
+
+for (n1 = 0;n1 < helloWorld.length; n1++ ) {
+
+
+
+    console.log(helloWorld[time])
+    
+    time = time + 1
+
+}
+// Code for Dipslaing helloworld in alphabetical order-----------------------------------------------------
+
+n2 = helloWorld.sort();
+console.log(n2);
+
+
+
